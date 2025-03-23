6faf5600124871cd2995c629cd1cdf4d43bf7021
yamaguchi1113 <23238474@edu.cc.saga-u.ac.jp>
2025-03-24 02:00:18 +0900

    branch-a commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 import ShoppingList from './ShoppingList.jsx'
 
 const data = [
-  {item: '卵', quantity: 12, completed: false},
-  {item: '牛乳', quantity: 1, completed: false},
-  {item: 'パン', quantity: 1, completed: true},
-  {item: 'バター', quantity: 1, completed: false},
+  {id: 1, item: '卵', quantity: 12, completed: false},
+  {id: 2, item: '牛乳', quantity: 1, completed: false},
+  {id: 3, item: 'パン', quantity: 1, completed: true},
+  {id: 4, item: 'バター', quantity: 1, completed: false},
 ];
 
 function App() {
--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -2,7 +2,11 @@ export default function ShoppingList({items}) {
     return (
         <ul>
             {items.map(i =>
-                <li style = {{ color: i.completed ? 'red' : 'black' }}>
+                <li 
+                key={i.id}
+                style = {{ color: i.completed ? 'red' : 'green', 
+                textDecoration: i.completed ? 'line-through' : 'none' 
+                }}>
                     { i.item } - {i.quantity}
                 </li>
             )}
