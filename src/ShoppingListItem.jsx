4f34fb87de876587bbb365880b762ac3f4b486ee
yamaguchi1113 <23238474@edu.cc.saga-u.ac.jp>
2025-03-28 18:58:36 +0900

    オンオフ機能の追加

--- src/ShoppingListItem.jsx
+++ src/ShoppingListItem.jsx
@@ -1,11 +1,12 @@
 import './ShoppingListItem.css';
 
-export default function ShoppingListItem({item, quantity, completed, imageUrl}) {
-    return(
-    <li className="shopping-list-item" 
-        style={{ color: completed ? 'black' : 'red',
-        textDecoration: completed ? 'line-through' : 'none' }}>
-        <img src={imageUrl} alt={item} className="item-image" />
-        {item}:<br/>{quantity}
-    </li>)
+export default function ShoppingListItem({id, item, quantity, completed, imageUrl, completedItem}) {
+    return (
+        <li className="shopping-list-item" 
+            onClick={() => completedItem(id)} 
+            style={{ textDecoration: completed ? 'line-through' : 'none' }}>
+            <img src={imageUrl} alt={item} className="item-image" />
+            {item}:<br/>{quantity}
+        </li>
+    )
 }
